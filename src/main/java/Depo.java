b6257ef11e9aa67321909fec1b36481fe6c8e0c5
BinnurAG <f.binnur@gmail.com>
2024-03-04 23:57:52 +0100

    depo projesi

--- src/main/java/Depo.java
+++ src/main/java/Depo.java
@@ -1,67 +1,91 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Depo {
-    private final List<Urun> urunListesi;
+class Depo {
+    private Map<Integer, Urun> urunler;
+    LocalDateTime myCurrentTime = LocalDateTime.now();
+    DateTimeFormatter time = DateTimeFormatter.ofPattern("dd-MMM-yyyy  hh:mm a");
+    String formattedTime = time.format(myCurrentTime);
 
+    static int idSayac = 1000;
     public Depo() {
-        this.urunListesi = new ArrayList<>();
+        this.urunler = new HashMap<>();
     }
 
-    public void urunTanimlama(int id, String urunIsmi, String uretici, String birim) {
-        Urun urun = new Urun(id, urunIsmi, uretici, 0, birim, 0);
-        urunListesi.add(urun);
+
+    public void urunTanimlama(String urunIsmi, String uretici, String birim) {
+        Urun urun = new Urun(urunIsmi, uretici, 0, birim, null);
+
+        urun.setId(idSayac++);
         System.out.println("Ürün tanımlandı: " + urunIsmi);
+        urunler.put(urun.getId(), urun);
     }
 
-    public void urunListele() {
+    public void urunListele() { //Hüseyin hocadan alalım
+
         System.out.println("Ürün Listesi:");
-        for (Urun urun : urunListesi) {
-            System.out.println("ID: " + urun.getId() +
-                    ", Ürün İsmi: " + urun.getUrunIsmi() +
-                    ", Üretici: " + urun.getUretici() +
-                    ", Miktar: " + urun.getMiktar() +
-                    ", Birim: " + urun.getBirim() +
-                    ", Raf: " + urun.getRaf());
+        System.out.printf("%-8s%-12s%-15s%-12s%-12s%-8s%n", "ID", "ISIM", "URETICI", "MIKTAR", "BIRIM", "RAF");
+
+        for (Urun urun : urunler.values()) {
+
+            System.out.printf("%-8s%-12s%-15s%-12s%-12s%-8s%n", urun.getId(),
+                     urun.getUrunIsmi(),
+                     urun.getUretici(),
+                     urun.getMiktar(),
+                     urun.getBirim(),
+                     urun.getRaf());
+        }
+
+    }
+/* public void urunListele() {
+    System.out.printf("%-8s%-12s%-15s%-12s%-12s%-8s%n", "ID", "ISIM", "URETICI", "MIKTAR", "BIRIM", "RAF");
+    System.out.println("---------------------------------------------------------------");
+    for (Urun product : urunList) {
+        System.out.printf("%-8s%-12s%-15s%-12s%-12s%-8s%n", product.id, product.urunIsmi, product.uretici, product.miktar, product.birim, product.raf);}
+    System.out.println();
+}*/
+
+
+
+
+    public void urunGirisi(int id, int girisMiktar) {
+        Urun urun = urunler.get(id);
+        if (urun != null) {
+            int yeniMiktar = urun.getMiktar() + girisMiktar;
+            urun.setMiktar(yeniMiktar);
+            System.out.println("Ürün girişi yapıldı. Yeni miktar: " + yeniMiktar + " Giriş zamanı "+ formattedTime);
+        } else {
+            System.out.println("Ürün bulunamadı.");
         }
     }
 
-    public void urunGirisi(int id, int miktar) {
-        for (Urun urun : urunListesi) {
-            if (urun.getId() == id) {
-                int yeniMiktar = urun.getMiktar() + miktar;
+
+    public void urunuRafaKoy(int id, String raf) {
+        Urun urun = urunler.get(id);
+        if (urun != null) {
+            urun.setRaf(raf);
+            System.out.println("Ürün rafa konuldu.");
+        }else
+            System.out.println("Ürün bulunamadı.");
+
+    }
+
+    public void urunCikisi(int id, int cikisMiktar) {
+        Urun urun = urunler.get(id);
+        if (urun != null) {
+            int yeniMiktar = urun.getMiktar() - cikisMiktar;
+            if (yeniMiktar >= 0) {
                 urun.setMiktar(yeniMiktar);
-                System.out.println("Ürün girişi yapıldı. Yeni miktar: " + yeniMiktar);
-                return;
+                System.out.println("Ürün çıkışı yapıldı. Yeni miktar: " + yeniMiktar + "  Çıkış zamanı "+ formattedTime);
+            } else {
+                System.out.println("Stokta yeterli ürün yok.");
             }
+        }else {
+            System.out.println("Ürün bulunamadı.");
         }
-        System.out.println("Ürün bulunamadı.");
-    }
 
-    public void urunuRafaKoy(int id, int raf) {
-        for (Urun urun : urunListesi) {
-            if (urun.getId() == id) {
-                urun.setRaf(String.valueOf(raf));
-                System.out.println("Ürün rafa konuldu.");
-                return;
-            }
-        }
-        System.out.println("Ürün bulunamadı.");
-    }
-
-    public void urunCikisi(int id, int miktar) {
-        for (Urun urun : urunListesi) {
-            if (urun.getId() == id) {
-                int yeniMiktar = urun.getMiktar() - miktar;
-                if (yeniMiktar >= 0) {
-                    urun.setMiktar(yeniMiktar);
-                    System.out.println("Ürün çıkışı yapıldı. Yeni miktar: " + yeniMiktar);
-                } else {
-                    System.out.println("Stokta yeterli ürün yok.");
-                }
-                return;
-            }
-        }
-        System.out.println("Ürün bulunamadı.");
     }
 }
--- src/main/java/DepoUygulamasi.java
+++ src/main/java/DepoUygulamasi.java
@@ -1,11 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.function.ToDoubleBiFunction;
 
 public class DepoUygulamasi {
-    public static void main(String[] args) {
+
+
+
+
+    public static void main(String[] args) throws InterruptedException{
         Scanner scanner = new Scanner(System.in);
         Depo depo = new Depo();
 
+        // TODO: 04/03/2024 aynı ürün geldiğinde, bu ürün mevcuttur diye uyarı versin, ürün eklemeye devam etmek için 1 e çıkmak için 9 a basın
+
+
+
+
+
+
+
+
+
+        //mismatch exception ı kullanacak mıyız?
         while (true) {
+
+
+
+            System.out.println("\nDepo Uygulaması");
             System.out.println("1- Ürün Tanımlama");
             System.out.println("2- Ürün Listeleme");
             System.out.println("3- Ürün Girişi");
@@ -13,49 +34,61 @@ public class DepoUygulamasi {
             System.out.println("5- Ürün Çıkışı");
             System.out.println("6- Çıkış");
             System.out.print("Seçiminizi yapın: ");
-            int secim = scanner.nextInt();
+
+
+            int secim;
+            try {
+                secim = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Geçersiz giriş! Lütfen bir tamsayı girin.");
+                scanner.next(); // Boşluk karakterlerini temizler
+                continue; // Döngünün başına dön
+            }
+
 
             switch (secim) {
-                case 1 -> {
-                    System.out.print("ID: ");
-                    int id = scanner.nextInt();
-                    scanner.nextLine(); // Dummy nextLine to consume newline character
+                case 1:
+
+                    scanner.nextLine();
                     System.out.print("Ürün İsmi: ");
                     String urunIsmi = scanner.nextLine();
                     System.out.print("Üretici: ");
                     String uretici = scanner.nextLine();
                     System.out.print("Birim: ");
                     String birim = scanner.nextLine();
-                    depo.urunTanimlama(id, urunIsmi, uretici, birim);
-                }
-                case 2 -> depo.urunListele();
-                case 3 -> {
+                    depo.urunTanimlama(urunIsmi, uretici, birim);
+                    break;
+                case 2:
+                    depo.urunListele();
+                    break;
+                case 3:
                     System.out.print("ID: ");
                     int girisId = scanner.nextInt();
                     System.out.print("Miktar: ");
                     int girisMiktar = scanner.nextInt();
                     depo.urunGirisi(girisId, girisMiktar);
-                }
-                case 4 -> {
+                    break;
+                case 4:
                     System.out.print("ID: ");
                     int rafId = scanner.nextInt();
                     System.out.print("Raf: ");
-                    int raf = scanner.nextInt();
+                    String raf = scanner.next();
                     depo.urunuRafaKoy(rafId, raf);
-                }
-                case 5 -> {
+                    break;
+                case 5:
                     System.out.print("ID: ");
                     int cikisId = scanner.nextInt();
                     System.out.print("Miktar: ");
                     int cikisMiktar = scanner.nextInt();
                     depo.urunCikisi(cikisId, cikisMiktar);
-                }
-                case 6 -> {
-                    System.out.println("Çıkış yapılıyor...");
+                    break;
+                case 6:
+                    System.out.println("Programdan çıkılıyor...");
                     scanner.close();
                     System.exit(0);
-                }
-                default -> System.out.println("Geçersiz seçim. Tekrar deneyin.");
+                default:
+                     System.out.println("Geçersiz seçim. Tekrar deneyin.");
+
             }
         }
     }
